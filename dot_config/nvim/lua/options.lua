f166c64ad9a533cd7bedccbbe58ff22680e48246
Chloe Hutchinson <30171259+chloeruka@users.noreply.github.com>
2026-04-23 12:52:33 +1000

    Tmux tweaks for pane styling

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -8,3 +8,5 @@ o.cursorlineopt = "both" -- to enable cursorline!
 o.linebreak = true -- break at whitespace instead of last character on screen
 o.tabstop = 4
 o.softtabstop = 4
+o.title = true -- emit terminal title (for tmux pane)
+o.titlestring = "nvim" -- lowercase, matches other pane labels
